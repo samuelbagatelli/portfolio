c6f5c9d491a2b5a44e3c5061f76b5b6834619dc6
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-11-06 02:21:53 -0300

    Instalação de algumas libs de css e embelezamento da página

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 import Header from './components/Header';
 import Presentation from './components/Presentation';
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <Presentation />
-    </main>
+    <SimpleBar forceVisible="y" style={{ maxHeight: '100vh' }} autoHide={false}>
+      <main>
+        <Header />
+        <Presentation />
+      </main>
+    </SimpleBar>
   );
 }
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Presentation() {
   return (
-    <section className="ml-5">
-      <p className="font-oswald font-bold text-7xl">
+    <section className="ml-5 py-20">
+      <p className="font-oswald font-bold text-7xl pb-12">
         Olá,
         <br />
         me chamo Samuel,
@@ -12,7 +12,7 @@ export default function Presentation() {
         <br />
         FullStack
       </p>
-      <button className="border border-teal-300 px-8 py-2 text-teal-300">
+      <button className="border border-teal-300 px-8 py-2 text-teal-300 transition-contact">
         Entre em contato!
       </button>
     </section>
