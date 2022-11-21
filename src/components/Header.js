a864c4d4392f427085ecf5debe051c503ce872de
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-11-21 10:50:54 -0300

    Portfolio finalizado, não refatorado

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,24 @@ export default function Header() {
   return (
     <header className="fixed flex one-hundred-percent justify-between my-2">
       <a href="#home">
-        <AiOutlineHome className="text-5xl text-white bg-neutral-900 opacity-80 transition rounded p-2 hover:bg-neutral-200 hover:text-black" />
+        <AiOutlineHome className="text-5xl text-white bg-neutral-900 opacity-80 transition rounded p-2 hover:bg-neutral-200 hover:text-black correction" />
       </a>
+      {
+        !hambMenu
+        && <div className="flex flex-col bg-neutral-900 full-left opacity-90 rounded-lg transition">
+          <a className="hamb-menu mb-1">Home</a>
+          <a className="hamb-menu mb-1">Projetos</a>
+          <a className="hamb-menu mb-1">Sobre</a>
+          <a className="hamb-menu">Contato</a>
+        </div>
+      }
       <button
         onClick={() => setHambMenu(!hambMenu)}
       >
         {
           hambMenu
             ? <GiHamburgerMenu className="text-5xl text-white bg-neutral-900 opacity-80 transition rounded p-2 hover:bg-neutral-200 hover:text-black float-right" />
-            : <AiOutlineClose className="text-5xl text-white bg-neutral-900 opacity-80 transition rounded p-2 hover:bg-neutral-200 hover:text-black float-right" />
+            : <AiOutlineClose className="text-5xl mb-40 text-white bg-neutral-900 opacity-80 transition rounded p-2 hover:bg-neutral-200 hover:text-black float-right" />
         }
       </button>
     </header>
